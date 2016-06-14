a1513598fc6f01a11d47627dcbb513d5993a39a9
lingtran <lxiutran@gmail.com>
2016-06-14 12:00:16 -0600

    Create index view for search results
    Create index view for search results
    WIP take out zip code params showing in the URL for search

--- spec/features/user_can_search_for_zipcode_spec.rb
+++ spec/features/user_can_search_for_zipcode_spec.rb
@@ -12,18 +12,18 @@ RSpec.describe "User can search by zipcode", type: :feature do
     end
 
     expect(page).to have_current_path '/search'
-    expect(page).to have_content("Stores within 25 miles of searched zipcode")
-    expect(page).to have_content("17 Total Stores")
-    expect(page).to have_content("Showing: 15 total stores")
-    expect(page).to have_content("Next Page")
 
     within("#store-info") do
       expect(page).to have_content("Long Name")
       expect(page).to have_content("City")
       expect(page).to have_content("Distance")
       expect(page).to have_content("Phone Number")
-      expect(page).to have_content("Storey Type")
+      expect(page).to have_content("Store Type")
     end
 
+    expect(page).to have_content("Stores within 25 miles of searched zipcode")
+    expect(page).to have_content("17 Total Stores")
+    expect(page).to have_content("Showing: 15 total stores")
+    expect(page).to have_content("Next Page")
   end
 end
