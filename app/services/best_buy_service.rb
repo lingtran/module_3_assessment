2bf111d325760635096cb7af41ce19d7eaf015d3
lingtran <lxiutran@gmail.com>
2016-06-14 10:44:17 -0600

    start structuring service

---
+++ app/services/best_buy_service.rb
@@ -0,0 +1,22 @@
+class BestBuyService
+  def initialize
+    @_connection = Faraday.new(url: "https://api.bestbuy.com/v1/")
+  end
+
+  def get_stores_by_zipcode_and_location(zipcode, location)
+    connection.get "stores(area(#{zipcode}, #{location}))", { format: "json", apiKey: ENV['best_buy_api_key'], show: "longName, city,  distance, phone, storeType"}
+  end
+
+  def parse_stores_by_zipcode_and_location(zipcode, location)
+    parse(get_stores_by_zipcode_and_location(zipcode, location))
+  end
+
+  def parse(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  private
+  def connection
+    @_connection
+  end
+end
